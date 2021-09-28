750283e709026bf53ddcd22cb0611366ac50121d
Ron Itelman <ron@intelligence.ai>
2021-09-28 09:16:07 -0600

    input all tests passed

--- public/javascripts/hyperdata/json_input.js
+++ public/javascripts/hyperdata/json_input.js
@@ -1,46 +1,109 @@
 hyper.json_input.init = function() {
+  let {input, meta} = hyper.json_input.getElems();
+  hyper.json_input.updateMetaVal({meta, input});  
+  hyper.json_input.addFocusListener({ input, meta});
+  hyper.json_input.addBlurListener({ input, meta });
+  hyper.json_input.addKeyListener({ input, meta });
+};
+
+hyper.json_input.getElems = function() {
   let meta = document.querySelector('#jsonMetaInput');
   let input = document.querySelector(`#jsonInput textarea`);
-  input.addEventListener('focus', function() {
-    let valid = hyper.json_input.checkIfValidJson({input, val:meta.value});
-    if (valid) {
-      gsap.to(input, { backgroundColor: "rgb(196, 255, 215)", borderColor: "green", duration: 0.3 });
+  return {meta, input};
+};
+
+hyper.json_input.updateMetaVal = function(params = {}) {
+  let {meta, input} = params;
+  if (input.value != undefined) {
+    meta.value = input.value;
+  }
+  else {
+    meta.value = "";
+  }
+  return meta.value;
+};
+
+hyper.json_input.addFocusListener = function(params = {}) {
+  let {input, meta} = params;
+  input.addEventListener('focus', function () {
+    if (meta.value != undefined && meta.value != null && meta.value != "") {
+      console.log(meta.value);
+      console.log(meta.value == undefined);
+      console.log(typeof meta.value);
+      let valid = hyper.json_input.checkIfValidJson({input, meta});
+      hyper.json_input.setInputColors({valid, input});
     }
+    
+    
   });
-  input.addEventListener('keyup', function() {    
-    meta.value = input.value;    
-    if (meta.value) {
-      
-      try {
-        let obj = JSON.parse(meta.value);
-        gsap.to(input, { backgroundColor:"rgb(196, 255, 215)", borderColor:"green", duration:0.3});
-      }
-      catch(err) {
-        gsap.to(input, { backgroundColor:"rgb(255, 215, 215)", borderColor:"red", duration:0.3});
-      }        
+  
+};
+
+hyper.json_input.resetInputColors = function(params = {}) {
+  let {input} = params;
+  gsap.to(input, { backgroundColor: "rgb(255,255,255)", borderColor: "#ccc", duration: 0.3 });
+};
+
+hyper.json_input.addBlurListener = function(params = {}) {
+  let {input, meta} = params;
+  input.addEventListener('blur', function () {
+    let valid = hyper.json_input.checkIfValidJson({input, meta});
+    if (valid) {
+      console.log('valid');
+      hyper.json_input.resetInputColors({input});
     }
     else {
-      gsap.to(input, { backgroundColor: "rgb(255, 255,255)", borderColor: "#ccc", duration: 0.3 });
+      console.log('invalid');
+      hyper.json_input.setInputColors({valid, input});
+    }
+
+  });
+
+};
+
+hyper.json_input.addKeyListener = function(params={}) {
+  let {input, meta} = params;
+  
+  input.addEventListener('keyup', function () {
+    let meta_val = hyper.json_input.updateMetaVal({meta, input});
+    if (meta_val != "") {
+      console.log('has input', meta_val);
+      let valid = hyper.json_input.checkIfValidJson({input, val:meta.val});
+      console.log(valid);
+      hyper.json_input.setInputColors({input, valid});
+      
+    }    
+    else {
+      console.log('no input');
+      hyper.json_input.resetInputColors({input});
     }
   });
-  input.addEventListener('blur', function() {
-    let valid = hyper.json_input.checkIfValidJson({ input, val: meta.value });
-    if (valid || !meta.value) {
-      gsap.to(input, { backgroundColor:"rgb(255, 255,255)", borderColor:"#ccc", duration:0.3});
-    }    
-  });
+
+};
+
+
+hyper.json_input.setInputColors = function(params={}) {
+  let {valid, input} = params;
+  if (valid) {
+    gsap.to(input, { backgroundColor: "rgb(196, 255, 215)", borderColor: "green", duration: 0.3 });
+  }
+  else {
+    gsap.to(input, { backgroundColor: "rgb(255, 215, 215)", borderColor: "red", duration: 0.3 });
+  }
 };
 
 hyper.json_input.checkIfValidJson = function(params={}) {
-  let {input, val} = params;
+  let {input, meta} = params;
   try {
-    let obj = JSON.parse(val);
-    gsap.to(input, { backgroundColor: "rgb(196, 255, 215)", borderColor: "green", duration: 0.3 });
-    return true;
+    if (input.value) {
+      let obj = JSON.parse(input.value);    
+      return true;
+    }
+    else {
+      return true;
+    }
   }
-  catch (err) {
-    console.log('invalue');
-    gsap.to(input, { backgroundColor: "rgb(255, 215, 215)", borderColor: "red", duration: 0.3 });
+  catch (err) {        
     return false;
   }
 };
