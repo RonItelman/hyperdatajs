f3e6384460ed69b1ec98ffa5d3045ef3d33a9f0b
Ron Itelman <ron@intelligence.ai>
2021-10-15 11:17:28 -0600

    upgrading the UX of navigating through entities

--- public/javascripts/hyperdata/views/json/init.js
+++ public/javascripts/hyperdata/views/json/init.js
@@ -1,4 +1,6 @@
 hyper.views.json.init = function() {  
+  let textarea = document.querySelector("#jsonTextArea");
+  textarea.value = JSON.stringify(window.JSON_IMPORT, null, 4);
   hyper.views.json.elems.init();
   hyper.views.json.input.init();  
   hyper.views.json.editor.init();  
