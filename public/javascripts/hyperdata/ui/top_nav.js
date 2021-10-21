d52208d6273626ca66629ee6b833a73de5d9d98c
Ron Itelman <ron@intelligence.ai>
2021-10-21 06:37:48 -0600

    ok

--- public/javascripts/hyperdata/ui/top_nav.js
+++ public/javascripts/hyperdata/ui/top_nav.js
@@ -15,9 +15,22 @@ hyper.ui.top_nav.setSelected = function(opt) {
   opt.setAttribute('data-state', 'active');
 };
 
+hyper.ui.top_nav.setCurrentView = function(selector) {
+  console.log('selector', selector);
+  let view = document.querySelector(`${selector}`);
+  view.setAttribute('data-state', 'selected');
+};
+
+hyper.ui.top_nav.hideCurrentView = function(){
+  let current = document.querySelector('.viewW[data-state="selected"]');
+  current.setAttribute('data-state', 'hidden');
+};
+
 hyper.ui.top_nav.addListener = function(opt) {
   opt.addEventListener('click', function() {
     hyper.ui.top_nav.clearSelector();
     hyper.ui.top_nav.setSelected(opt);
+    hyper.ui.top_nav.hideCurrentView();
+    hyper.ui.top_nav.setCurrentView(opt.getAttribute('data-view'));
   });
 };
