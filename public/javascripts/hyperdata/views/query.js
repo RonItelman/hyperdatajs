4998e1b3a559b2b4afe8a3e345689b092211d105
Ron Itelman <ron@intelligence.ai>
2021-10-21 12:53:39 -0600

    almost

--- public/javascripts/hyperdata/views/query.js
+++ public/javascripts/hyperdata/views/query.js
@@ -1,23 +1,58 @@
 hyper.views.query.addInputListener = function() {
   let input = document.querySelector('#queryInputW input');
+  
+  let social = document.querySelector('#labelsW .labelW[data-schema_id="135"]');
+  
+  let fire = document.querySelector('#labelsW .labelW[data-schema_id="159"]');
+  
   input.addEventListener('keyup', function(event) {
     let val = input.value;
     val.toLowerCase();
-    if (val == "pla") {
-      console.log('show planetphobia')
+    console.log(val);
+    if (val == "pla" || val == "plan" || val == "plane" || val == "planet" || val == "cli" || val == "clim" || val == "clima" || val == "climat" || val == "climate") {
+      let elem = document.querySelector('#labelsW .labelW[data-schema_id="135"]');;
+      console.log('show planet', elem);
+      gsap.set(elem, {display:"flex"});
+      gsap.to(elem, {opacity:1, duration:0.1});
     }
-    if (val == "soc") {
-      console.log('show social')
+    else {
+      
+      let elem = document.querySelector('#labelsW .labelW[data-schema_id="135"]');
+      console.log('show planet', elem);
+    
+      gsap.set(elem, {display:"none", delay:0.1});
+      gsap.to(elem, {opacity:0, duration:0.1});
     }
-    if (val == "com") {
-      console.log('show comb')
+    if (val == "soc" || val == "soci" || val == "socia" || val == "social" || val == "consp") {
+      let elem = document.querySelector('#labelsW .labelW[data-schema_id="147"]');;
+      console.log('show planet', elem);
+      gsap.set(elem, { display: "flex" });
+      gsap.to(elem, { opacity: 1, duration: 0.1 });
     }
-    if (val == "clima") {
-      console.log('show planet')
+    else {
+      let elem = document.querySelector('#labelsW .labelW[data-schema_id="147"]');
+      console.log('show planet', elem);
+
+      gsap.set(elem, { display: "none", delay: 0.1 });
+      gsap.to(elem, { opacity: 0, duration: 0.1 });
+      
     }
-    if (val == "conspi") {
-      console.log('show planet')
+    if (val == "fire" || val == "com" || val == "spo") {
+      let elem = document.querySelector('#labelsW .labelW[data-schema_id="159"]');
+      console.log('show planet', elem);
+
+      gsap.set(elem, { display: "flex" });
+      gsap.to(elem, { opacity: 1, duration: 0.1 });
     }
+    else {
+      let elem = document.querySelector('#labelsW .labelW[data-schema_id="159"]');
+      console.log('show planet', elem);
+
+      gsap.set(elem, { display: "none", delay: 0.1 });
+      gsap.to(elem, { opacity: 0, duration: 0.1 });
+
+    }
+    
   });
 };
 
